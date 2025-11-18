3e5103091e26a9805ab59b4660b8869535b3f0f8
Maurice Daniel Hiponia <maurice.hiponia@gmail.com>
2025-11-18 15:02:12 +0800

    refactor: add direct module imports, and improve get root directory

--- src/server.js
+++ src/server.js
@@ -1,40 +1,47 @@
-import {
-  readFile,
-  join,
-  extname,
-  createServer,
-} from "./config/dependencies.js";
+import { readFile } from "node:fs/promises";
+import { join, extname } from "node:path";
+import { createServer } from "node:http";
+import dotenv from "dotenv";
 
-const MIME = {
-  ".html": "text/html",
-  ".css": "text/css",
-  ".js": "text/javascript",
-  ".json": "application/json",
-};
+import { getRootDir } from "./helpers/rootDir.js";
+import { getMimeType } from "./helpers/mimeType.js";
+import { getProducts } from './api/getProduct.js';
 
-const getFilePath = (reqUrl) => {
-  const pathname = new URL(reqUrl, "http://localhost").pathname;
-  return join(
-    "./../public",
-    pathname === "/" ? "index.html" : decodeURIComponent(pathname),
-  );
-};
+const rootDir = await getRootDir;
+dotenv.config({ path: join(rootDir, '.env'), quiet: true});
 
-const server = createServer(async (req, res) => {
+const HOST = process.env.HOSTNAME;
+const PORT = process.env.PORT;
+
+const server = createServer(async (request, response) => {
+  if (request.url === '/') {
+    try {
+      const file = await readFile(join(rootDir, 'public', 'index.html'), 'utf-8');
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      return response.end(file);
+    } catch (err) {
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      return response.end('Error loading index.html');
+    }
+  }
+
+  if (request.url === '/api/product') {
+    const products = await getProducts();
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(products));
+  }
+
+  const filePath = join(rootDir, 'public', request.url);
   try {
-    const path = getFilePath(req.url);
-    const data = await readFile(path);
-    const type = MIME[extname(path)] || "application/octet-stream";
-    res.writeHead(200, { "Content-Type": type });
-    res.end(data);
+    const data = await readFile(filePath);
+    const mimeType = getMimeType(extname(filePath));
+    response.writeHead(200, { 'Content-Type': mimeType });
+    return response.end(data);
   } catch {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    return response.end('File not found');
   }
 });
-
-const HOST = process.env.HOSTNAME || "127.0.0.1";
-const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}`);
